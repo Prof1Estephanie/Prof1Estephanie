cc6433ef31e10e49f6061375743e674721fad43b
Prof1Estephanie <estephanie.ferreira@escola.pr.gov.br>
2024-08-14 21:06:32 +0000

    Alteração Turma FT 14/08

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ const perguntas =[
                 afirmacao: "Resposta positiva.  Facilitar a entrada e a saída de um público específico." 
             },
             {
-            texto: "Não ou raramente." ,
-            afirmacao: "Resposta negativa. Manter os assentos e as entradas do transporte público no modelo atual."
+                texto: "Não ou raramente." ,
+                afirmacao: "Resposta negativa. Manter os assentos e as entradas do transporte público no modelo atual."
             }
         ]
     },
@@ -22,12 +22,12 @@ const perguntas =[
         enunciado : "O desmatamento no Brasil atingiu índices alarmantes., tornando-se praticamente cultural. Os efeitos estão sendo sentidos no dia-a-dia da população. É possível minimizar o desmatamento no país?",
         alternativas : [
             {
-            texto: "Sim, por intermédio de políticas de fiscalização e controle ambiental rigoroso",
-            afirmacao: "Com a rigidez da fiscalização podemos induzir a mudança de comportamento das pessoas por meio de sanções."
+                texto: "Sim, por intermédio de políticas de fiscalização e controle ambiental rigoroso",
+                afirmacao: "Com a rigidez da fiscalização podemos induzir a mudança de comportamento das pessoas por meio de sanções."
             },
             {
-            texto: "Não, porque as leis não são cumpridas" ,
-            afirmacao: "Há deficiência no sistema de fiscalização." 
+                texto: "Não, porque as leis não são cumpridas" ,
+                afirmacao: "Há deficiência no sistema de fiscalização." 
             }
         ]
     },
@@ -35,12 +35,12 @@ const perguntas =[
         enunciado : "Qual as consequências do uso de agrotóxico na agricultura?",
         alternativas : [
             {
-            texto: "Há um impacto muito grande no meio ambiente, causando prejuízos ao solo, água e atmosfera. ",
-            afirmacao: "Causando a morte das abelhas, diminuindo a diversidade das plantas, gerando um impacto econômico."
+                texto: "Há um impacto muito grande no meio ambiente, causando prejuízos ao solo, água e atmosfera. ",
+                afirmacao: "Causando a morte das abelhas, diminuindo a diversidade das plantas, gerando um impacto econômico."
             },
             {
-            texto: "A produção das espécies  é maior, reduz o controle de pragas, aumenta a distribuição da produção." ,
-            afirmacao: "Aumentando a produção há uma vantagem no setor de exportação, favorecendo assim a economia do país."
+                texto: "A produção das espécies  é maior, reduz o controle de pragas, aumenta a distribuição da produção." ,
+                afirmacao: "Aumentando a produção há uma vantagem no setor de exportação, favorecendo assim a economia do país."
             }
         ]
     }
@@ -56,16 +56,21 @@ function mostraPerguntas(){
     mostraAlternativas() ;
 } ;
 
+
 mostraPerguntas() ;
 
 function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button") ;
         botaoAlternativas.textContent = alternativa.texto ;
-        botaoAlternativas.addEventListener("click", function(){
-        atual++ ;    
-        mostraPerguntas() ;
-        }) ;
+        botaoAlternativas.addEventListener("click", ()=>respostaFinal(alternativa)) ;
         caixaAlternativas.appendChild(botaoAlternativas) ;
-    }
+    } ;
 } ;
+
+function respostaFinal(opcaoSelecionda){
+    const afirmacao = opcaoSelecionada.afirmacao ;
+    historiaFinal   = afirmacao ;
+    atual++ ;
+    mostraPerguntas() ;    
+} ; 
